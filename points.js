b10ec73e138074134dcfac0d6d32c9997cf87741
Maksim <monpase12@gmail.com>
2023-05-22 14:32:38 +0700

    add new solution to point

--- points.js
+++ points.js
@@ -1,7 +1,18 @@
-function points(games) {
-    let sum = 0;
-    for (let i = 0; i < games.length; i++){
-        sum += games[i][0] > games[i][2] ? 3 : games[i][0] == games[i][2] ? 1 : 0;
-    }
-    return sum;
-}
+// function points(games) {
+//     let sum = 0;
+//     for (let i = 0; i < games.length; i++){
+//         sum += games[i][0] > games[i][2] ? 3 : games[i][0] == games[i][2] ? 1 : 0;
+//     }
+//     return sum;
+// }
+
+const pointsUp = (games) => {
+  return games.reduce(
+    (value, oldValue, index, games) =>
+      value +
+      (oldValue[0] > oldValue[2] ? 3 : oldValue[0] == oldValue[2] ? 1 : 0),
+    0
+  );
+};
+
+console.log(pointsUp(["1:0", "2:0", "3:3"]));
---
+++ test.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const arr = [1, 2, 3, 4, 5, 6, 7];
+let sum = arr.reduce((a, b) => a + b, 0);
+console.log(sum);
